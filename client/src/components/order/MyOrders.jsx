2cd5ec490947a57a3710ebe38e54823b8134c931
Franco Valledor <franco.valledor@bluealba.com>
2023-05-09 12:13:50 -0300

    add filter to MyOrders

--- client/src/components/order/MyOrders.jsx
+++ client/src/components/order/MyOrders.jsx
@@ -1,10 +1,24 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 
+const filterOrders = (allOrders, searchTerm) => {
+    if (!searchTerm) return allOrders;
+
+    return allOrders.filter((o) => 
+        `${o.userName}
+        ${o.userAddress}
+        ${o.products.map((p) => p.name).join(" ")}
+        ${o.state}`
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase()));
+};
+
 export default function MyOrders() {
     const token = localStorage.getItem("token");
     const [userId, setUserId] = useState()
     const [data, setData] = useState([])
+    const [searchTerm, setSearchTerm] = useState("");
+    const filteredOrders = filterOrders(data, searchTerm);
 
     useEffect(() => {
         if (token) {
@@ -29,32 +43,52 @@ export default function MyOrders() {
         }
     }, [userId]);
 
-
   return (
-    <div className='mt-5'>
-        {
-                data !== undefined
-                    ?
-                    <div style={{displat:'flex', justifyContent:'center', textAlign:'center'}}>
-                        {data.map(order => (
-                            <div>
-                            <p>
-                                <a class="btn btn-primary" data-bs-toggle="collapse" href={('#').concat(order._id)} role="button" aria-expanded="false" aria-controls="collapseExample">Pedido #{order._id} - {order.state} - Fecha {order.date_added.substr(0, 10)} </a>
-                            </p>
-                            <div class="collapse" id={order._id}>
-                                {order.products.map(product => (
-                                    <div class="card card-body">
-                                    <p>{product.name} - {product.quantity}</p>
-                                    </div>
-                                ))}
-                            </div>
-                            </div>
-                        ))}
-                    </div>
-                    :
-                    <p>No hay ningún pedido en el historial</p>
-                
-            }
+    <div className="mt-5">
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      {data !== undefined ? (
+        <div
+          style={{
+            displat: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+          }}
+        >
+          {filteredOrders.map((order) => (
+            <div key={order._id}>
+              <p>
+                <a
+                  className="btn btn-primary"
+                  data-bs-toggle="collapse"
+                  href={"#".concat(order._id)}
+                  role="button"
+                  aria-expanded="false"
+                  aria-controls="collapseExample"
+                >
+                  Pedido #{order._id} - {order.state} - Fecha{" "}
+                  {order.date_added.substr(0, 10)}{" "}
+                </a>
+              </p>
+              <div className="collapse" id={order._id}>
+                {order.products.map((product) => (
+                  <div className="card card-body" key={product._id}>
+                    <p>
+                      {product.name} - {product.quantity}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No hay ningún pedido en el historial</p>
+      )}
     </div>
-  )
+  );
 }
