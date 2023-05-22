54e26c4b97348fef5a05bd5ee74295d10fe987c3
Mateo <burismateo@gmail.com>
2023-05-22 10:21:07 -0300

    Delete server/auth directory

--- server/auth/auth0.config.js
+++
@@ -1,8 +0,0 @@
-module.exports.config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:8080',
-  clientID: 'Vrjx5LJzoFam7A1jGdLmm0zmepxI1dnL',
-  issuerBaseURL: 'https://dev-ekmyptzm.us.auth0.com'
-};
